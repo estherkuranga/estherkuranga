299928af673ed3ee0b6cade7f32fc77f752f5bff
Kuranga Esther Adepelumi <estherak64@gmail.com>
2026-04-05 20:27:40 +0100

    Refactor Testimonials component: adjust slidesPerView and image dimensions for improved layout

--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -3,17 +3,9 @@ import React, { use } from "react";
 import SectionHeader from "./SectionHeader";
 import { testimonialData } from "@/lib/data";
 import Image from "next/image";
-import clsx from "clsx";
 
 // import Swiper core and required modules
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Autoplay,
-} from "swiper/modules";
-
+import { Pagination, A11y, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -30,17 +22,14 @@ export default function Testimonials() {
     >
       <Swiper
         // install Swiper modules
-        modules={[Navigation, Pagination, A11y, Autoplay]}
-        spaceBetween={24}
-        slidesPerView={1.25}
-        // slidesPerView={{clsx("1.25", "sm:1.5", "md:2", "lg:2.5", "xl:3")}}
-        // navigation
-        autoplay
+        modules={[Pagination, A11y, Autoplay]}
+        spaceBetween={42}
+        slidesPerView={1.4}
+        autoplay={{ delay: 2000 }}
         pagination={{ clickable: true }}
-        // scrollbar={{ draggable: false }}
         onSwiper={(swiper) => console.log(swiper)}
         onSlideChange={() => console.log("slide change")}
-        className="mt-4 w-4/6 max-sm:w-full m-auto "
+        className="mt-4 w-5/6 max-sm:w-full m-auto "
       >
         {testimonialData.map((testimonial, index) => (
           <SwiperSlide
@@ -51,8 +40,8 @@ export default function Testimonials() {
               <Image
                 src={testimonial.avatar}
                 alt={testimonial.name}
-                width={80}
-                height={80}
+                width={90}
+                height={90}
                 className="rounded-full sm:w-25 sm:h-25"
               />
               <div className="w-full flex flex-col ">
@@ -67,95 +56,6 @@ export default function Testimonials() {
             </div>
           </SwiperSlide>
         ))}
-
-        {/* 1 */}
-        {/* <SwiperSlide className="w-full bg-[#f8f8f8] rounded-lg flex  gap-4 justify-between items-center mb-4 sm:mb-5">
-          <div className="w-full flex max-sm:flex-col max-sm:justify-center gap-4 justify-between items-center p-4">
-            <Image
-              src={testimonialData[0].avatar}
-              alt={testimonialData[0].name}
-              width={80}
-              height={80}
-              className="rounded-full sm:w-25 sm:h-25"
-            />
-
-            <div className="w-full flex flex-col ">
-              <p className="text-[#424242]">{testimonialData[0].feedback}</p>
-              <h5 className="text-lg font-bold tracking-[0.63px] mt-1 text-black dark:text-[#FBFBFB]">
-                {testimonialData[0].name}
-              </h5>
-              <p className="text-sm text-gray-500 tracking-[0.38px]">
-                {testimonialData[0].title}
-              </p>
-            </div>
-          </div>
-        </SwiperSlide> */}
-        {/* 2 */}
-        {/* <SwiperSlide className="mb-4 w-full bg-[#f8f8f8] rounded-lg flex  gap-4 justify-between items-center">
-          <div className="w-full flex max-sm:flex-col max-sm:justify-center gap-4 justify-between items-center p-4">
-            <Image
-              src={testimonialData[1].avatar}
-              alt={testimonialData[1].name}
-              width={90}
-              height={90}
-              className="rounded-full sm:w-25 sm:h-25"
-            />
-
-            <div className="w-full flex flex-col ">
-              <p className="text-[#424242]">{testimonialData[1].feedback}</p>
-              <h5 className="text-lg font-bold tracking-[0.63px] mt-1 text-black dark:text-[#FBFBFB]">
-                {testimonialData[1].name}
-              </h5>
-              <p className="text-sm text-gray-500 tracking-[0.38px]">
-                {testimonialData[1].title}
-              </p>
-            </div>
-          </div>
-        </SwiperSlide> */}
-        {/* 3 */}
-        {/* <SwiperSlide className="w-full bg-[#f8f8f8] rounded-lg flex  gap-4 justify-between items-center">
-          <div className="w-full flex max-sm:flex-col max-sm:justify-center gap-4 justify-between items-center p-4">
-            <Image
-              src={testimonialData[0].avatar}
-              alt={testimonialData[0].name}
-              width={90}
-              height={90}
-              className="rounded-full sm:w-25 sm:h-25"
-            />
-
-            <div className="w-full flex flex-col">
-              <p className="text-[#424242]">{testimonialData[0].feedback}</p>
-              <h5 className="text-lg font-bold tracking-[0.63px] mt-1 text-black dark:text-[#FBFBFB]">
-                {testimonialData[0].name}
-              </h5>
-              <p className="text-sm text-gray-500 tracking-[0.38px]">
-                {testimonialData[0].title}
-              </p>
-            </div>
-          </div>
-        </SwiperSlide> */}
-        {/* 4 */}
-        {/* <SwiperSlide className="w-full bg-[#f8f8f8] rounded-lg flex  gap-4 justify-between items-center">
-          <div className="w-full flex max-sm:flex-col max-sm:justify-center gap-4 justify-between items-center p-4">
-            <Image
-              src={testimonialData[2].avatar}
-              alt={testimonialData[2].name}
-              width={90}
-              height={90}
-              className="rounded-full sm:w-25 sm:h-25"
-            />
-
-            <div className="w-full flex flex-col">
-              <p className="text-[#424242]">{testimonialData[2].feedback}</p>
-              <h5 className="text-lg font-bold tracking-[0.63px] mt-1 text-black dark:text-[#FBFBFB]">
-                {testimonialData[2].name}
-              </h5>
-              <p className="text-sm text-gray-500 tracking-[0.38px]">
-                {testimonialData[2].title}
-              </p>
-            </div>
-          </div>
-        </SwiperSlide> */}
       </Swiper>
     </SectionHeader>
   );
